e9b94ec773d5e538f4b33f531e2ba117124d33f3
maurya050 <mauryashivam050@gmail.com>
2025-08-19 19:37:57 +0530

    design update

--- src/components/ChatBotApp.jsx
+++ src/components/ChatBotApp.jsx
@@ -9,23 +9,24 @@ const ChatBotApp = () => {
                 <h2>Chat List</h2>
                 <i className='bx bx-edit-alt new-chat'></i>
             </div>
-            <div className="chat-list-item">
+            <div className="chat-list-item active">
                 <h4>Chat 20/07/2024 12:23:53 PM</h4>
-                <i className="bx bx-x circle"></i>
+                <i className="bx bx-x-circle"></i>
             </div>
+            <div className="chat-list-item ">
+                <h4>Chat 20/07/2024 12:23:53 PM</h4>
+                <i className="bx bx-x-circle"></i>
+            </div> 
             <div className="chat-list-item">
                 <h4>Chat 20/07/2024 12:23:53 PM</h4>
-                <i className="bx bx-x circle"></i>
-            </div> <div className="chat-list-item">
-                <h4>Chat 20/07/2024 12:23:53 PM</h4>
-                <i className="bx bx-x circle"></i>
+                <i className="bx bx-x-circle"></i>
             </div>
 
         </div>
         <div className="chat-window">
             <div className="chat-title">
-                <h3>Chat with AI</h3>
-                <i className="bx-bx-arrow-back arrow"></i>
+                <h3>T@stBot</h3>
+                <i className="bx bx-chevron-right arrow"></i>
             </div>
             <div className="chat">
                 <div className="prompt">Hi, How are you?
@@ -37,7 +38,7 @@ const ChatBotApp = () => {
                 <div className="typing">Typing...</div>
             </div>
             <form className='msg-form'>
-                <i className="fa-solid fa-face smile emoji"></i>
+                <i className="fa-solid fa-face-smile emoji"></i>
                 <input type="text" className='msg-input' placeholder='Type your message here...' />
                 <i className="fa-solid fa-paper-plane"></i>
                 {/* <button type='submit'><i className='bx bx-send'></i></button> */}
